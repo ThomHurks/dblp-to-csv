12afc8c301211961b95a361146b2132291d3330f
Thom Hurks <thomhurks@gmail.com>
2016-03-14 20:40:35 +0100

    - Add a unique id “id” to the rows of each generated file. Right now is simply equal to the row number within that file. - If the input file already contains a main tag attribute “id” or a sub element “id”, then stop and print an error, since otherwise this would cause a conflict. - Small var rename

--- XMLToCSV.py
+++ XMLToCSV.py
@@ -9,6 +9,18 @@ import time
 __author__ = 'Thom Hurks'
 
 
+class InvalidElementName(Exception):
+    def __init__(self, invalid_element_name, tag_name, parent_name):
+        self.invalid_element_name = invalid_element_name
+        self.tag_name = tag_name
+        self.parent_name = parent_name
+
+    def __str__(self):
+        return "Invalid name %s found in tag %s within element %s" % (repr(self.invalid_element_name),
+                                                                      repr(self.tag_name),
+                                                                      repr(self.parent_name))
+
+
 def existing_file(filename):
     if os.path.isfile(filename):
         return filename
@@ -40,10 +52,11 @@ def open_outputfiles(elements, element_attributes, output_filename):
     (path, ext) = os.path.splitext(output_filename)
     output_files = dict()
     for element in elements:
-        outputpath = "%s_%s%s" % (path, element, ext)
-        outputfile = open(outputpath, "w")
+        output_path = "%s_%s%s" % (path, element, ext)
+        output_file = open(output_path, "w")
         fieldnames = sorted(list(element_attributes[element]))
-        output_writer = csv.DictWriter(outputfile, fieldnames=fieldnames, delimiter=';', quoting=csv.QUOTE_NONNUMERIC,
+        fieldnames.insert(0, "id")
+        output_writer = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter=';', quoting=csv.QUOTE_NONNUMERIC,
                                        restval='')
         output_writer.writeheader()
         output_files[element] = output_writer
@@ -63,12 +76,17 @@ def get_element_attributes(xml_file, elements):
             current_tag = elem.tag
             keys = elem.keys()
             if len(keys) > 0:
+                keys = set(keys)
+                if "id" in keys:
+                    raise InvalidElementName("id", elem.tag, "root")
                 attributes = data.get(current_tag, set())
-                data[current_tag] = attributes.union(set(keys))
+                data[current_tag] = attributes.union(keys)
         elif current_tag is not None and event == "end":
             if elem.tag == current_tag:
                 current_tag = None
             elif elem.tag is not None and elem.text is not None:
+                if elem.tag == "id":
+                    raise InvalidElementName("id", elem.tag, current_tag)
                 attributes = data.get(current_tag, set())
                 attributes.add(elem.tag)
                 keys = elem.keys()
@@ -89,6 +107,9 @@ def parse_xml(xml_file, elements, output_files):
     data = dict()
     current_tag = None
     multiple_valued_cells = set()
+    unique_ids = dict()
+    for key in output_files.keys():
+        unique_ids[key] = -1
     for event, elem in context:
         if current_tag is None and event == "start" and elem.tag in elements:
             current_tag = elem.tag
@@ -99,7 +120,10 @@ def parse_xml(xml_file, elements, output_files):
             if elem.tag == current_tag:
                 for cell in multiple_valued_cells:
                     data[cell] = ' | '.join(sorted(data[cell]))
-                output_files[current_tag].writerow(data)
+                if len(data) > 0:
+                    row_id = unique_ids[current_tag] = unique_ids[current_tag] + 1
+                    data["id"] = row_id
+                    output_files[current_tag].writerow(data)
                 current_tag = None
             elif elem.tag is not None and elem.text is not None:
                 set_cell_value(data, elem.tag, elem.text, multiple_valued_cells)
@@ -131,7 +155,12 @@ def main():
             elements = get_elements(dtd_file)
         with open(args.xml_filename, "rb") as xml_file:
             print("Finding unique attributes for all elements...")
-            element_attributes = get_element_attributes(xml_file, elements)
+            try:
+                element_attributes = get_element_attributes(xml_file, elements)
+            except InvalidElementName as e:
+                element_attributes = None
+                print(e)
+                exit(1)
         print("Opening output files...")
         output_files = open_outputfiles(elements, element_attributes, args.outputfile)
         with open(args.xml_filename, "rb") as xml_file:
